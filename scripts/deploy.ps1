9c4b2245086cf8ec48fcbb16e4c1e1ba3257696e
Your Name <daniel@kryptonunite.com>
2020-11-06 02:51:30 +0000

    Bump version: 0.1.0 → 1.0.0

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 python setup.py sdist bdist_wheel
-$version="0.1.0"
+$version="1.0.0"
 $files_to_handle_str="dist/flights_time_series_dataset-$version*" 
 twine check $files_to_handle_str
 twine upload $files_to_handle_str
