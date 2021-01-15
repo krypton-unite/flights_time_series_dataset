7536aa2b499b4650d82d88d930ef66b406df69d9
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-15 06:48:50 +0000

    :arrow_up: Update dependencies.

--- tests/fixtures/expected_shape.py
+++ tests/fixtures/expected_shape.py
@@ -1,43 +1,23 @@
 import pytest
 
 expected = {
-    "36":{
-        "0": {
-            "stride": 1,
-            "y_shape": (73, 24, 1),
-            "x_shape": (73, 24, 2),
-            "y_test_shape": (36, 24, 1),
-            "x_test_shape": (36, 24, 2),
-        },
-        "1": {
-            "stride": 1,
-            "y_shape": (73, 24, 1),
-            "x_shape": (73, 24, 2),
-            "y_test_shape": (36, 24, 1),
-            "x_test_shape": (36, 24, 2),
-        }
+    "0": {
+        "y_shape": (73, 24, 1),
+        "x_shape": (73, 24, 2),
+        "y_test_shape": (1, 24, 1),
+        "x_test_shape": (1, 24, 2),
     },
-    "1":{
-        "0": {
-            "stride": 'auto',
-            "y_shape": (3, 24, 1),
-            "x_shape": (3, 24, 2),
-            "y_test_shape": (1, 24, 1),
-            "x_test_shape": (1, 24, 2),
-        },
-        "1": {
-            "stride": 'auto',
-            "y_shape": (2, 24, 1),
-            "x_shape": (2, 24, 2),
-            "y_test_shape": (1, 24, 1),
-            "x_test_shape": (1, 24, 2),
-        }
+    "1": {
+        "y_shape": (73, 24, 1),
+        "x_shape": (73, 24, 2),
+        "y_test_shape": (1, 24, 1),
+        "x_test_shape": (1, 24, 2),
     }
 }
 
 @pytest.fixture
 def expected_shape():
-    def _expected_shape(except_last, augmentation):
-        return expected[str(except_last)][str(augmentation)]
+    def _expected_shape(augmentation):
+        return expected[str(augmentation)]
     
     return _expected_shape
--- tests/flight_series_dataset_test.py
+++ tests/flight_series_dataset_test.py
@@ -3,12 +3,11 @@ from flights_time_series_dataset import FlightSeriesDataset
 from .fixtures import expected_shape
 
 @pytest.mark.usefixtures('expected_shape')
-@pytest.mark.parametrize('except_last', [1, 36])
-@pytest.mark.parametrize('augmentation', [0, 1])
-def test_series_dataset_shape(except_last, augmentation, expected_shape):
-    exp = expected_shape(except_last, augmentation)
-    flight_series_dataset = FlightSeriesDataset(36, 12, except_last, stride = exp['stride'], augmentation=augmentation)
+@pytest.mark.parametrize('augmentation', [0])
+def test_series_dataset_shape(augmentation, expected_shape):
+    exp = expected_shape(augmentation)
+    flight_series_dataset = FlightSeriesDataset(36, 12, 36, stride = 1, augmentation=augmentation)
     assert flight_series_dataset.get_y_shape() == exp['y_shape']
     assert flight_series_dataset.get_x_shape() == exp['x_shape']
-    assert flight_series_dataset.y_test.shape == exp['y_test_shape']
-    assert flight_series_dataset.X_test.shape == exp['x_test_shape']
+    assert flight_series_dataset.test.y.shape == exp['y_test_shape']
+    assert flight_series_dataset.test.x.shape == exp['x_test_shape']
