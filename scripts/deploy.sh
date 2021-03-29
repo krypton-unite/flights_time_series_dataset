e671f4bdfd50b62a0711742adf401bd91978c88e
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-03-29 19:15:21 +0100

    :arrow_up: Update dependencies.

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+python setup.py sdist bdist_wheel
+version="1.1.9"
+files_to_handle_str="dist/flights_time_series_dataset-$version*" 
+twine check $files_to_handle_str
+twine upload $files_to_handle_str
